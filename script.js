d9759299d15c4cd50d154e50a863b1d2f460574b
Tasha <latashasalgado@gmail.com>
2019-02-07 08:58:12 -0600

    Portfolio Completed

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 /* script for index and projects*/
-
  $(document).ready(function() {
- $('.hamburger-btn').click(function() {
- if ($('.nav-mobile').css('display') == 'none') {
- $('.nav-mobile').slideDown('slow');
- $('body').css('overflow', 'hidden');
- } else {
- $('.nav-mobile').slideUp('slow');
- $('body').css('overflow', 'auto');
- }
- });
+   $('.hamburger-btn').click(function() {
+     if ($('.nav-mobile').css('display') == 'none') {
+       $('.nav-mobile').slideDown('slow');
+       $('body').css('overflow', 'hidden');
+     } else {
+       $('.nav-mobile').slideUp('slow');
+       $('body').css('overflow', 'auto');
+     }
+   });
+   $('.nav-mobile a').click(function() {
+     $('body').css('overflow', 'auto');
+   });
  });
