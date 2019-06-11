76931d9edc0dd63e10bbb5201c9bc5867bf9699e
Tasha <latashasalgado@gmail.com>
2019-06-11 07:31:32 -0500

    fixed scrolling issue in js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-/* script for index and projects*/
+// /* script for index and projects*/
 
 
-  $(document).ready(function() {
-    $('.hamburger-btn').click(function() {
-      if ($('.nav-mobile').css('display') == 'none') {
-        $('.nav-mobile').slideDown('slow');
-        $('body').css('overflow', 'hidden');
-      } else {
-		  $('#connect_btn').fadeToggle(500);
-        $('.nav-mobile').slideUp('slow');
-        $('body').css('overflow', 'auto');
-      }
-    });
-	
-	/* this is for connect button fadin */
-	$('#connect').on('click',function(e){ 
-		e.preventDefault();   
-		$('#connect_btn').fadeIn(1000);
-	});
-  });
+$(document).ready(function() {
+$('.hamburger-btn').click(function() {
+if ($('.nav-mobile').css('display') == 'none') {
+$('.nav-mobile').slideDown('slow');
+$('body').css('overflow', 'hidden');
+} else {
+$('.nav-mobile').slideUp('slow');
+$('body').css('overflow', 'auto');
+}
+});
+
+$(document).on('click', 'a[href^="#"]', function(e) {
+e.preventDefault();
+$('.nav-mobile').slideUp('fast');
+$('body').css('overflow', 'auto');
+var dest = $(this).attr('href');
+$('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
+});
+});
